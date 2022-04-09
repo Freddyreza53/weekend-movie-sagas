3c76440b1793204b3147b4c6b365a03329046657
Freddy Reza <rezafreddy@gmail.com>
2022-04-09 12:53:40 -0500

    show clicked movie on details page

--- src/components/DetailsPage/DetailsPage.jsx
+++ src/components/DetailsPage/DetailsPage.jsx
@@ -1,8 +1,17 @@
+import { useHistory } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 
 function DetailsPage () {
+    const movie = useSelector(store => store.clickedMovie)
+
     return (
-        <h1>Details of movie go here</h1>
+        <div>
+            <h3>{movie.title}</h3>
+            <img onClick={() => handleClick(movie)} src={movie.poster} alt={movie.title}/>
+            <p>{movie.description}</p>
+        </div>
+
     )
 }
 
