02299c40333504cc0a236fb4d263a2bfb4d9bc15
Freddy Reza <rezafreddy@gmail.com>
2022-04-10 12:47:49 -0500

    remove and rearrange some lines of code

--- src/components/DetailsPage/DetailsPage.jsx
+++ src/components/DetailsPage/DetailsPage.jsx
@@ -4,17 +4,7 @@ import { useSelector } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
-const useStyles = makeStyles({
-    root: {
-        maxWidth: 400,
-
-    },
-});
-
 function DetailsPage () {
-
-    const classes = useStyles();
-
     const movie = useSelector(store => store.clickedMovie)
     const history = useHistory();
     
@@ -23,27 +13,21 @@ function DetailsPage () {
     }
 
     return (
-        
         <div className="movieBackground">
             <h1 className="movieTitle">{movie.title}</h1>
             <div className="movieDiv">
-                
                 <img src={movie.poster} alt={movie.title}/>
                 <div className="movieDescription">
-                {movie.genres.map(genre => {
-                    return (
-                        <h4 className="genresList" key={genre.name}> {genre.name} </h4>
-                    )
-                })}
-                
+                    {movie.genres.map(genre => {
+                        return (
+                            <h4 className="genresList" key={genre.name}> {genre.name} </h4>
+                        )
+                    })}
                     <p>{movie.description}</p>
                 </div>
             </div>
             <Button variant="outlined" className= "backButton" onClick={handleClick}>GO BACK TO MOVIE LIST</Button>
-        
-            
         </div>
-
     )
 }
 
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -6,9 +6,7 @@ import { useHistory } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Typography from '@material-ui/core/Typography';
 // import for carousel effect
 import Carousel from 'react-elastic-carousel';
 
@@ -19,7 +17,6 @@ const useStyles = makeStyles({
 });
 
 function MovieList() {
-
     const classes = useStyles();
 
     const dispatch = useDispatch();
@@ -55,11 +52,6 @@ function MovieList() {
                                 title={movie.title}
                                 onClick={() => handleClick(movie)}
                                 />
-                                {/* <CardContent>
-                                <Typography gutterBottom variant="h5" component="h2">
-                                    {movie.title}
-                                </Typography>
-                                </CardContent> */}
                             </CardActionArea>
                         </Card> 
                         </div>
