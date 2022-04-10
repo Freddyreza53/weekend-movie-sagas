c8a90c94caaa3bb99c00877f5e30c263f1a475a1
Freddy Reza <rezafreddy@gmail.com>
2022-04-10 08:42:00 -0500

    add MUI cards to movie list view

--- src/components/DetailsPage/DetailsPage.jsx
+++ src/components/DetailsPage/DetailsPage.jsx
@@ -10,15 +10,16 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
-// const useStyles = makeStyles({
-//     root: {
-//         maxWidth: 345,
-//     },
-// });
+const useStyles = makeStyles({
+    root: {
+        maxWidth: 400,
+
+    },
+});
 
 function DetailsPage () {
 
-    // const classes = useStyles();
+    const classes = useStyles();
 
     const movie = useSelector(store => store.clickedMovie)
     const history = useHistory();
@@ -40,36 +41,7 @@ function DetailsPage () {
             <p>{movie.description}</p>
             <button onClick={handleClick}>GO BACK TO MOVIE LIST</button>
         
-        
-
-            {/* <Card className={classes.root}>
-                <CardActionArea>
-                    <CardMedia
-                    component="img"
-                    alt="Contemplative Reptile"
-                    height="140"
-                    image="/static/images/cards/contemplative-reptile.jpg"
-                    title="Contemplative Reptile"
-                    />
-                    <CardContent>
-                    <Typography gutterBottom variant="h5" component="h2">
-                        Lizard
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary" component="p">
-                        Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                        across all continents except Antarctica
-                    </Typography>
-                    </CardContent>
-                </CardActionArea>
-                <CardActions>
-                    <Button size="small" color="primary">
-                    Share
-                    </Button>
-                    <Button size="small" color="primary">
-                    Learn More
-                    </Button>
-                </CardActions>
-            </Card> */}
+            
         </div>
 
     )
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -2,8 +2,26 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './MovieList.css'
 import { useHistory } from 'react-router-dom';
+// imports for styling with Material UI
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+    root: {
+        maxWidth: 165,
+
+    },
+});
 
 function MovieList() {
+
+    const classes = useStyles();
 
     const dispatch = useDispatch();
     const history = useHistory();
@@ -27,10 +45,27 @@ function MovieList() {
             <section className="movies">
                 {movies.map(movie => {
                     return (
-                        <div key={movie.id} >
-                            <h3>{movie.title}</h3>
-                            <img onClick={() => handleClick(movie)} src={movie.poster} alt={movie.title}/>
-                        </div>
+                        <Card className={classes.root} key={movie.id}>
+                            <CardActionArea>
+                                <CardMedia
+                                component="img"
+                                alt={movie.title}
+                                height="250"
+                                image={movie.poster}
+                                title={movie.title}
+                                onClick={() => handleClick(movie)}
+                                />
+                                <CardContent>
+                                <Typography gutterBottom variant="h5" component="h2">
+                                    {movie.title}
+                                </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card> 
+                        // <div key={movie.id} >
+                        //     <h3>{movie.title}</h3>
+                        //     <img onClick={() => handleClick(movie)} src={movie.poster} alt={movie.title}/>
+                        // </div>
                     );
                 })}
             </section>
@@ -39,3 +74,4 @@ function MovieList() {
 }
 
 export default MovieList;
+
